ad6e9de80d8b7437c2807e4ac484b7a4ab315c79
nyinyilayhtet <nnlayhtet@gmail.com>
2022-04-28 10:11:12 -0400

    commit tree c000835e0d5f3e348576fbcacc4042065372af60

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+//function superbowlWin(array){
+
+const record = [
+        { year: "2015", result: "W"},
+        { year: "2014", result: "N/A"},
+        { year: "2013", result: "L"},
+        //...
+      ]
+      
+superbowlWin = (record) => {
+    const won = record.find( record => record.result === "W" );
+    return !!won ? won.year : undefined;
+}
+
